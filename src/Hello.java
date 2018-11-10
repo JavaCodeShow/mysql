ef623c592e054ec487ae5b0969d5f3adcc1e8291
jiangfeng <1546844168@qq.com>
2018-11-10 21:48:11 +0800

    do better

--- src/Hello.java
+++ src/Hello.java
@@ -3,7 +3,9 @@
  * @create 2018-11-09 9:37
  */
 public class Hello {
-    public static void main(String[] args){
-        System.out.println("对MySql进行更深层次的学习");
+
+    public static void main(String[] args)
+    {
+        System.out.println(6&1);
     }
 }
